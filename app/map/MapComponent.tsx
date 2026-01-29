25b29130da45831726f29ab3e83046510c698fa9
krittichitr <krittichitr@gmail.com>
2026-01-30 04:58:57 +0700

    fix leaflet marker re-render

--- app/map/MapComponent.tsx
+++ app/map/MapComponent.tsx
@@ -1,43 +1,46 @@
-'use client'
+"use client";
 
-import { MapContainer, TileLayer, Marker } from 'react-leaflet'
-import { useEffect, useState } from 'react'
-import axios from 'axios'
-import L from 'leaflet'
+import { MapContainer, TileLayer, Marker } from "react-leaflet";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import L from "leaflet";
 
 type Position = {
-  lat: number
-  lng: number
-}
+  lat: number;
+  lng: number;
+};
 
 export default function MapComponent() {
-  const [pos, setPos] = useState<Position | null>(null)
+  const [pos, setPos] = useState<Position | null>(null);
 
   useEffect(() => {
     const interval = setInterval(async () => {
       try {
-        const res = await axios.get<Position>('/api/push-location')
-        setPos(res.data)
+        const res = await axios.get<Position>("/api/push-location");
+        setPos({
+          lat: res.data.lat,
+          lng: res.data.lng,
+        });
       } catch (err) {
-        console.log('fetch location error', err)
+        console.log("fetch location error", err);
       }
-    }, 3000)
+    }, 3000);
 
-    return () => clearInterval(interval)
-  }, [])
+    return () => clearInterval(interval);
+  }, []);
 
-  if (!pos) return <div>Loading map...</div>
+  if (!pos) return <div>Loading map...</div>;
 
   return (
     <MapContainer
       center={[pos.lat, pos.lng]}
       zoom={16}
-      style={{ height: '100vh', width: '100%' }}
+      style={{ height: "100vh", width: "100%" }}
     >
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <Marker position={[pos.lat, pos.lng]} />
+      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      {pos && (
+        <Marker key={`${pos.lat}-${pos.lng}`} position={[pos.lat, pos.lng]} />
+      )}
     </MapContainer>
-  )
+  );
 }
