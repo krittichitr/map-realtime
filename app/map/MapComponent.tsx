71eaca6bde9ec7d3c6b61850f21f68872b6f2fb4
krittichitr <krittichitr@gmail.com>
2026-01-30 05:00:37 +0700

    fix leaflet marker icon

--- app/map/MapComponent.tsx
+++ app/map/MapComponent.tsx
@@ -3,7 +3,22 @@
 import { MapContainer, TileLayer, Marker } from "react-leaflet";
 import { useEffect, useState } from "react";
 import axios from "axios";
-import L from "leaflet";
+
+import L from 'leaflet'
+import 'leaflet/dist/leaflet.css'
+
+// 🔥 FIX leaflet marker icon บน Next.js / Vercel
+delete (L.Icon.Default.prototype as any)._getIconUrl
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl:
+    'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png',
+  iconUrl:
+    'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
+  shadowUrl:
+    'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
+})
+
 
 type Position = {
   lat: number;
